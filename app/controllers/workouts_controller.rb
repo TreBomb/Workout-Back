ae4268278803b9b29e341ac7239bd3596fbbb5f5
Trestin <trestinishak@gmail.com>
2021-11-16 11:09:28 -0600

    hot fix to workouts index

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,7 @@
 class WorkoutsController < ApplicationController
     def index
         if params[:user_id]
-            @workouts = Workout.where(user_id: params[:user_id])
+            @workouts = Workout.where(user_id: params[:user_id]) include: [:exercises]
         else
             @workouts = Workout.all
         end
