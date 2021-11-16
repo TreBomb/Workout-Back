0003a56ebb13e92a44e5673ddb154a681d7fb088
Trestin <trestinishak@gmail.com>
2021-11-16 13:55:30 -0600

    added fallback

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
   post '/exercise-filter', to: 'exercises#filter'
+
+  #Heroku fallback
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
