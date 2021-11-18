09b5200afc9eba8d028586c519da84d45a03252b
Trestin <trestinishak@gmail.com>
2021-11-18 17:50:56 -0600

    added password requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
+
+    PASSWORD_REQUIREMENTS = /\A
+    (?=.{8,})
+    (?=.*\d)
+    /x
+
     validates :name, uniqueness: true
+    validates :email, uniqueness: true
+    validates :password, format: PASSWORD_REQUIREMENTS
     has_many :weekly_routines
 end
