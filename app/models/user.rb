ee4fd724a9d9f509e7609ea054c43b1a81962fc9
Trestin <trestinishak@gmail.com>
2021-11-18 18:01:23 -0600

    removed username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
     (?=.*\d)
     /x
 
-    validates :name, uniqueness: true
     validates :email, uniqueness: true
     validates :password, format: PASSWORD_REQUIREMENTS
     has_many :weekly_routines
